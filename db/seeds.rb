d7765d4d17fdc4396f146828a8451a0e6d173d3f
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-03-05 17:05:01 -0300

    fixes seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 File.open("words.txt", "r").each_line do |line|
   word, classification = line.delete("\n").split('/')
-  sorted_word = word.split('').sort.join('')
+  sorted_word = I18n.transliterate(word).chars.sort.join
   Word.create(
     name:word,
     sorted_word: sorted_word,
